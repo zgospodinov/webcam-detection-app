ab5651701189895c79a094496721b9b6b7c45ecc
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-21 07:48:01 +0300

    Live video recording

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from webcam-detection-app!")
+import cv2
+import time
+
+video_capture = cv2.VideoCapture(0)
+time.sleep(1)
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    check, frame = video_capture.read()
+    if not check:
+        print("Failed to capture image")
+        break
+    cv2.imshow("Webcam Feed", frame)
+    
+    key = cv2.waitKey(1)
+    # Exit the loop if 'q' is pressed
+    if key == ord('q'):
+        break
+
+video_capture.release()
+
+
